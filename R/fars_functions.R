8f91df91ca14d12a98d4b786b304968506ca12b9
Dan Languedoc <dan.languedoc@gmail.com>
2018-01-04 20:52:18 -0500

    testthat fix

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -26,11 +26,13 @@
 #'
 #' @export
 fars_read <- function(filename) {
-  if(!file.exists(filename))
-    stop("file '", filename, "' does not exist")
+##  if(!file.exists(filename))
+##    stop("file '", filename, "' does not exist")
   data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
-  })
+##    readr::read_csv(filename, progress = FALSE)
+      readr::read_csv(system.file("extdata", filename, package="farsdata"), progress = FALSE)
+
+      })
   dplyr::tbl_df(data)
 }
 
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,4 @@
 library(testthat)
 library(farsdata)
-library(dplyr)
 
-
-test_that("make_filename function", expect_equal(make_filename(2015), "accident_2015.csv.bz2"))
-
-setwd(system.file("extdata", package = "farsdata"))
-
-test_that("fars_read() works correctly", {
-  expect_is(fars_read("accident_2015.csv.bz2"), "tbl_df")
-  expect_error(fars_read("accident_2016.csv.bz2"))
-})
+test_check("farsdata")
